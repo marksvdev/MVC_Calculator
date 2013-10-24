0cdc8b2cdc073fa4c1ddd5f9c7fcbb58cf10fba6
marksvdev <marksv.dev@gmail.com>
2013-10-24 13:39:12 +0200

    test push4

--- MVC_Test/src/CalcView.java
+++ MVC_Test/src/CalcView.java
@@ -5,7 +5,7 @@ import java.awt.event.*;
 
 class CalcView extends JFrame {
 	private static final long serialVersionUID = 1L;
-//as22
+//as222
     private JTextField 	userInputTf 	= new JTextField(5);
     private JTextField 	userInput2Tf 	= new JTextField(5);
     private JTextField 	totalTf     	= new JTextField(20);
